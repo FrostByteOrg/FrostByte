64617a867e0e14c607b554f4193600cf904ee24d
multimokia <multimokia3@gmail.com>
2023-01-19 16:42:48 -0500

    *most* things are fixed:tm:

--- pages/api/socketio.ts
+++ pages/api/socketio.ts
@@ -1,7 +1,7 @@
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "lib/database.types";
+import { createClient } from '@supabase/supabase-js';
+import { Database } from 'lib/database.types';
 
 const supabase = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -9,93 +9,129 @@ const supabase = createClient<Database>(
 );
 
 const socketIoHandler = (req: NextApiRequest, res: NextApiResponse) => {
-    // TODO: See about these null issues. How often are they occurring and how fixable is this
-    if (!res.socket.server.io) {
-        console.log("Starting socket.io");
+  // TODO: See about these null issues. How often are they occurring and how fixable is this
+  // @ts-expect-error temp for testing purposes. SEE: TODO above
+  if (!res.socket.server.io) {
+    console.log('Starting socket.io');
 
-        const io = new Server(res.socket.server);
+    // @ts-expect-error Like the above expect, based on TODO
+    const io = new Server(res.socket.server);
 
-        io.on("connection", (socket) => {
-            socket.on("userJoined", (user, serverId) => {
-                supabase.from("servers")
-                    .select("*")
-                    .eq("id", serverId)
-                    .single()
-                    .then(server => {
-                        if (server.error) {
-                            console.log(`Error adding ${user.username} to server ${serverId}: ${server.error}`);
-                            return;
-                        }
+    io.on('connection', (socket) => {
+      socket.on('userJoined', (user, serverId) => {
+        supabase
+          .from('servers')
+          .select('*')
+          .eq('id', serverId)
+          .single()
+          .then(server => {
+            if (server.error) {
+              console.error(`Error adding ${user.username} to server ${serverId}: ${server.error}`);
+              return;
+            }
 
-                        console.log(`User ${user.username} joined ${server.data.name}`);
-                        // TODO: Build out a System channel/default channel to announce user joins. Discord defaults this to #general
-                        // socket.join(channel.id.toString());
-                        // socket.to(channel.id.toString()).emit("serverBroadcastsUserJoin", user, channel);
-                    });
-            });
+            console.log(`User ${user.username} joined ${server.data.name}`);
+            // TODO: Build out a System channel/default channel to announce user joins. Discord defaults this to #general
+            // socket.join(channel.id.toString());
+            // socket.to(channel.id.toString()).emit('serverBroadcastsUserJoin', user, channel);
+          });
+      });
 
-            socket.on("userSendsMessage", (message) => {
-                supabase.from("messages")
-                    .insert({
-                        channel: message.channelId,
+      socket.on('userSendsMessage', (message) => {
+        supabase
+          .from('messages')
+          .insert({
+            content: message.content,
+            sent_time: message.timeSent,
+            author_id: message.author_id,
+            channel_id: message.channelId,
+          })
+          .select(`
+            *,
+            server_users (
+              nickname
+            ),
+            channels (
+              name
+            )
+          `)
+          .single()
+          .then((savedMessage) => {
+            if (savedMessage.error) {
+              console.error(savedMessage.error);
+              return;
+            }
 
-                    })
-                prisma.message.create({
-                    data: {
-                        content: message.content,
-                        isEdited: message.isEdited,
-                        timeSent: message.timeSent,
-                        authorId: message.authorId,
-                        channelId: message.channelId
-                    },
-                    include: { channel: true, author: true }
-                })
-                    .then((savedMessage) => {
-                        io.to(message.channelId.toString()).emit("serverBroadcastsUserSentMessage", savedMessage);
-                        console.log(`[${savedMessage.author.username} @ ${savedMessage.channel.name}]: ${savedMessage.content}`);
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                    });
-            });
+            io.to(message.channelId.toString()).emit('serverBroadcastsUserSentMessage', savedMessage);
+            // TODO: Sort out the relationship on this, we should only be returning a single channel/server_user object from the query
+            console.log(`[${savedMessage.data.server_users[0].nickname} @ ${savedMessage.data.channels[0].name}]: ${savedMessage.data.content}`);
+          });
+      });
 
-            socket.on("userLeft", (user, channelId) => {
-                prisma.channel.findUniqueOrThrow({ where: { id: channelId }})
-                    .then((channel) => {
-                        console.log(`User ${user.username} left ${channel.name}`);
-                        socket.leave(channel.id.toString());
-                        socket.to(channel.id.toString()).emit("serverBroadcastsUserLeave", user, channel);
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                    });
-            });
+      socket.on('userLeft', (user, serverId) => {
+        supabase
+          .from('server_users')
+          .delete()
+          .eq('server_id', serverId)
+          .eq('profile_id', user.id)
+          .select(`
+            *,
+            servers (
+              *
+            )
+          `)
+          .single()
+          .then((server_user) => {
+            if (server_user.error) {
+              console.error(server_user.error);
+              return;
+            }
 
-            socket.on("userConnected", (userId) => {
-                prisma.user.findUniqueOrThrow({ where: { id: userId }})
-                    .then((user) => {
-                        console.log(`User ${user.username} is online.`);
-                    });
+            console.log(`User ${user.username} left ${server_user.data.servers!.name}`);
+            socket.leave(server_user.data.server_id.toString());
+            socket.to(server_user.data.server_id.toString()).emit('serverBroadcastsUserLeave', user, server_user.data.servers);
+          });
+      });
 
-                socket.emit("serverBroadcastsUserConnected", userId);
-            });
+      socket.on('userConnected', (userId) => {
+        supabase
+          .from('profiles')
+          .select('username')
+          .then((username) => {
+            if (username.error) {
+              console.error(username.error);
+              return;
+            }
 
-            socket.on("userDisconnected", (userId) => {
-                prisma.user.findUniqueOrThrow({ where: { id: userId }})
-                    .then((user) => {
-                        console.log(`User ${user.username} is offline.`);
-                    });
+            console.log(`User ${username} is online.`);
+          });
 
-                socket.emit("serverBroadcastsUserDisconnected", userId);
-            });
-        });
-    }
+        socket.emit('serverBroadcastsUserConnected', userId);
+      });
+
+      socket.on('userDisconnected', (userId) => {
+        supabase
+          .from('profiles')
+          .select('username')
+          .then((username) => {
+            if (username.error) {
+              console.error(username.error);
+              return;
+            }
+
+            console.log(`User ${username} is offline.`);
+          });
+
+        socket.emit('serverBroadcastsUserDisconnected', userId);
+      });
+    });
+  }
 };
 
 export default socketIoHandler;
 
 export const config = {
-    api: {
-        bodyParser: false
-    }
+  api: {
+    bodyParser: false
+  }
 };
