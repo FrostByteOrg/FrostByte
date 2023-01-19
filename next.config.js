7d17ac143fba0420e83a4f44314ec1796d08df8b
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-01-19 10:36:23 -0500

    user middleware

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  async rewrites() {
+    return {
+      //might need to change afterFiles => fallback
+      afterFiles: [{ source: '/:path*', destination: '/_404/:path*' }],
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
