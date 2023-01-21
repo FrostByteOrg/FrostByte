923584344a55fc4770228e20ed43491e20192582
multimokia <multimokia3@gmail.com>
2023-01-21 14:36:34 -0500

    fix these two filenames for use in dev

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '@/types/supabase';
+import { Database } from '@/types/database.supabase';
 
 export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
