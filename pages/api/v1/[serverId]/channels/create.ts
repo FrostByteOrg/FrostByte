57aa0102ea4d9600d707ea5dc73a7835dc1c8bbf
multimokia <multimokia3@gmail.com>
2023-01-25 14:07:09 -0500

    route fixes, channel modification and deletion

--- pages/api/v1/[serverId]/channels/create.ts
+++ pages/api/v1/[serverId]/channels/create.ts
@@ -1,21 +1,21 @@
-import { createChannel } from '@/services/channels.service';
+import { createChannel, updateChannel } from '@/services/channels.service';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
+  // TODO: Require user to have permission to manage channels
+  let serverId: number;
 
+  try {
+    serverId = parseInt(req.query.serverId as string);
+  }
+
+  catch (err: any) {
+    return res.status(400).send({ error: 'Invalid server ID' });
+  }
+
+  // NOTE: All operations after this point require appropriate authorization
   if (method === 'POST') {
-    // TODO: Require user to have permission to create channels
-    let serverId: number;
-
-    try {
-      serverId = parseInt(req.query.serverId as string);
-    }
-
-    catch (err: any) {
-      return res.status(400).send({ error: 'Invalid server ID' });
-    }
-
     await createChannel(
       serverId,
       req.body.name,
