d4619e8e8cb819808b994076a44d60433e7ab2f6
multimokia <multimokia3@gmail.com>
2023-01-28 15:14:07 -0500

    Server service

---
+++ services/server.service.ts
@@ -0,0 +1,56 @@
+import { supabase } from '@/lib/supabaseClient';
+
+export async function createServer(name: string) {
+  const dbResp = await supabase.from('servers').insert({ name }).select().single();
+
+  if (dbResp.error) {
+    return dbResp;
+  }
+
+  // Now that we have a server, we need to create a default channel for it
+  await supabase
+    .from('channels')
+    .insert({ name: 'general', server_id: dbResp.data.id });
+
+  return dbResp;
+}
+
+type CreateServerResponse = Awaited<ReturnType<typeof createServer>>;
+export type CreateServerResponseSuccess = CreateServerResponse['data'];
+export type CreateServerResponseError = CreateServerResponse['error'];
+
+export async function getServers() {
+  return await supabase.from('servers').select('*');
+}
+
+type GetServersResponse = Awaited<ReturnType<typeof getServers>>;
+export type GetServersResponseSuccess = GetServersResponse['data'];
+export type GetServersResponseError = GetServersResponse['error'];
+
+export async function getServer(id: number) {
+  return await supabase.from('servers').select('*').eq('id', id);
+}
+
+type GetServerResponse = Awaited<ReturnType<typeof getServer>>;
+export type GetServerResponseSuccess = GetServerResponse['data'];
+export type GetServerResponseError = GetServerResponse['error'];
+
+export async function updateServer(id: number, name: string) {
+  return await supabase.from('servers').update({ name }).eq('id', id);
+}
+
+type UpdateServerResponse = Awaited<ReturnType<typeof updateServer>>;
+export type UpdateServerResponseSuccess = UpdateServerResponse['data'];
+export type UpdateServerResponseError = UpdateServerResponse['error'];
+
+export async function deleteServer(id: number) {
+  // First things first, we need to delete all channels associated with this server
+  await supabase.from('channels').delete().eq('server_id', id);
+
+  // Then we can delete the server itself
+  return await supabase.from('servers').delete().eq('id', id);
+}
+
+type DeleteServerResponse = Awaited<ReturnType<typeof deleteServer>>;
+export type DeleteServerResponseSuccess = DeleteServerResponse['data'];
+export type DeleteServerResponseError = DeleteServerResponse['error'];
