c4a4592899554fc572ca5b66e50f92deef105786
multimokia <multimokia3@gmail.com>
2023-02-22 14:12:22 -0500

    revert matcher

--- middleware.ts
+++ middleware.ts
@@ -42,14 +42,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!favicon.ico|_next).*)','/api/:path*'],
 };
