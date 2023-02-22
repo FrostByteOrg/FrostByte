85d94d1655947aa8efc838980148cadb546fe2b7
multimokia <multimokia3@gmail.com>
2023-02-22 17:59:31 -0500

    fix?

--- supabase/tests/database/messages_rls.test.sql
+++ supabase/tests/database/messages_rls.test.sql
@@ -1,5 +1,5 @@
 BEGIN;
-  SELECT plan(4);
+  SELECT plan(1);
 
   SELECT tests.create_supabase_user('test_member');
   SELECT tests.authenticate_as('test_user');
@@ -8,10 +8,10 @@ BEGIN;
   SELECT tests.rls_enabled('public', 'servers');
 
   -- Now if we try to fetch servers, we should get an empty array
-  SELECT tests.assert_array_length(
+  SELECT is_empty(
     (SELECT servers FROM public.servers),
     0,
-    'No servers should be returned'
+    'No servers should be returned for users not in any servers'
   );
 
   SELECT * FROM finish();
