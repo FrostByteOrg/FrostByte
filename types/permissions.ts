d0551cc4b8137f47f56cd3c6f6b29426e3fbf225
multimokia <multimokia3@gmail.com>
2023-02-27 13:52:44 -0500

    add TODO, clean docs a lil

--- types/permissions.ts
+++ types/permissions.ts
@@ -1,46 +1,61 @@
 export enum ServerPermissions {
   /**
+   * 0
    * No permissions.
    */
   NONE = 0,
 
   /**
+   * 1
    * The user is the owner of the server.
    */
   OWNER = 1 << 0,
 
   /**
+   * 2
    * Server administrator. Effectively contains all permissions aside from destructive operations such as delete server
    */
   ADMINISTRATOR = 1 << 1,
 
   /**
+   * 4
    * Manage server settings, including server name, description, and icon.
    */
   MANAGE_SERVER = 1 << 2,
 
   /**
+   * 8
    * Manage server channels, including creating, editing, and deleting channels.
    */
   MANAGE_CHANNELS = 1 << 3,
 
   /**
+   * 16
    * This permission allows the user to manage other users by editing their nickname
    */
   MANAGE_USERS = 1 << 4,
 
   /**
+   * 32
    * Manage server roles, including creating, editing, and deleting roles.
    */
   MANAGE_ROLES = 1 << 5,
 
   /**
+   * 64
    * Manage messages, allowing the user to delete other user's messages
    */
   MANAGE_MESSAGES = 1 << 6,
 
   /**
+   * 128
    * Manage invites, allowing the user to create, edit, and delete invites.
    */
   MANAGE_INVITES = 1 << 7,
+
+  /**
+   * 256
+   * Create invites, allowing the user to create invites.
+   */
+  CREATE_INVITES = 1 << 8,
 }
