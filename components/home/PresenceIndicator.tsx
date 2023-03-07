4853a34ea4695c05e070194d0fb6135329d7d3ae
multimokia <multimokia3@gmail.com>
2023-03-07 17:11:03 -0500

    fix the z

--- components/home/PresenceIndicator.tsx
+++ components/home/PresenceIndicator.tsx
@@ -6,15 +6,16 @@ export function PresenceIndicator({ userId }: { userId: string }) {
   return (
     <div
       className={`
+        relative
         float-left
         w-4
         h-4
         rounded-full
+        z-10
         ${channel.presenceState()[userId] ? 'bg-green-600' : 'bg-slate-500'}
         border-2
         -mt-3
         mr-2
-        z-10
         border-slate-800/50
       `}
     />
