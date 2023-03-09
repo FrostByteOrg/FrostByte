7819f726d28c45afe6408e0c0026ab562f46d674
multimokia <multimokia3@gmail.com>
2023-03-09 11:26:27 -0500

    rm extraneous type

--- types/dbtypes.ts
+++ types/dbtypes.ts
@@ -10,12 +10,8 @@ export type ServerUser = Database['public']['Tables']['server_users']['Row'];
 
 // Custom type modifications for client side
 export type UnsavedMessage = Omit<Message, 'id' | 'created_at' | 'sent_time' | 'is_edited' | 'is_pinned' | 'edited_time' | 'author_id'>;
-
 export type ChatMessageWithUser = Message & { profiles: User };
-
 export type ServersForUser =  { server_id: number } & { servers: Server };
-
-export type ServerForUser = GetServerForUserResponseSuccess | IStringIndexable;
 
 export interface IStringIndexable {
   [key: string]: any
