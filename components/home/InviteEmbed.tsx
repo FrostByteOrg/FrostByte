48bb61c5aa6ab2206bc98ddc548cae7719e4be8a
multimokia <multimokia3@gmail.com>
2023-03-10 17:05:07 -0500

    I FIXED THE F*CKING STYLES

--- components/home/InviteEmbed.tsx
+++ components/home/InviteEmbed.tsx
@@ -33,22 +33,51 @@ export function InviteEmbed({ invite_code }: { invite_code: string }) {
   }
 
   return (
-    <div className='flex flex-row items-center justify-center bg-slate-700 p-4 rounded-md border-solid border-2 border-slate-800/50 max-w-lg'>
-      <div className='flex flex-col justify-center space-y-2 items-start'>
-        <div className='text-xl font-semibold tracking-wide text-left'>
-          <OverflowMarquee content={`You have been invited to join ${invite.servers.name}!`} maxLength={50}/>
+    <div className="flex
+      flex-row
+      items-center
+      justify-center
+      bg-slate-700
+      p-4
+      rounded-md
+      border-solid
+      border-2
+      border-slate-800/50
+      w-full
+      max-w-xl
+      overflow-hidden
+    ">
+      <div className="flex flex-col justify-center space-y-2 items-start max-w-full">
+        <div className="text-xl font-semibold tracking-wide text-left overflow-hidden w-[45ch]">
+          <OverflowMarquee
+            content={`You have been invited to join ${invite.servers.name}!`}
+            maxLength={50}
+          />
         </div>
-        <div className='flex flex-row w-full space-x-3'>
-          <div className='flex-grow-0 bg-zinc-700 p-2 rounded-lg border-solid border-2 border-slate-800/50'>
-            <ServersIcon server={invite.servers} hovered={false} width={7} height={7}/>
+        <div className="flex flex-row w-full space-x-3 items-center">
+          <div className="flex-shrink-0 bg-zinc-700 p-2 rounded-lg border-solid border-2 border-slate-800/50 ">
+            <ServersIcon
+              server={invite.servers}
+              hovered={false}
+              width={7}
+              height={7}
+            />
           </div>
-          <div className='justify-center items-center flex-grow'>
-            {!!invite.servers.description && <div className='text-slate-400 font-semibold'>
-              <OverflowMarquee content={invite.servers.description} maxLength={90}/>
-            </div>}
-            <ServerMemberStats server={invite.servers} flexStyle='flex flex-row space-x-4 font-semibold items-center justify-center'/>
+          <div className="flex flex-col justify-center items-start px-2">
+            {!!invite.servers.description && (
+              <div className="text-slate-400 font-semibold w-[39ch] flex-grow">
+                <OverflowMarquee
+                  content={invite.servers.description}
+                  maxLength={40}
+                />
+              </div>
+            )}
+            <ServerMemberStats
+              server={invite.servers}
+              flexStyle="flex flex-row space-x-4 font-semibold items-center justify-center flex-grow"
+            />
           </div>
-          <button className='bg-green-700 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-md max-h-6 basis-8'>
+          <button className="bg-green-700 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-md max-h-6 flex-shrink-0 align-middle">
             Join
           </button>
         </div>
--- components/home/OverflowMarquee.tsx
+++ components/home/OverflowMarquee.tsx
@@ -10,14 +10,16 @@ export function OverflowMarquee({ content, maxLength }: { content: string, maxLe
         <span
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
+          className='inline-block overflow-hidden whitespace-nowrap max-w-full -mb-2'
         >
           {isHovered ? (
             <Marquee
               play={isHovered}
               direction={'left'}
               gradient={false}
+              className='w-full'
             >
-              {`${content}\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0`}
+              {`${content}\u00A0\u00A0\u00A0\u00A0`}
             </Marquee>
           ) : (`${content.slice(0, maxLength + 1)}...`)}
         </span>
