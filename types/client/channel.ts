026e71a3f99fd9ca2ec3fe2557e5fccba7f90fc0
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-03-24 15:12:41 -0400

    trim channel name

--- types/client/channel.ts
+++ types/client/channel.ts
@@ -3,7 +3,8 @@ import { object, TypeOf, z } from 'zod';
 export const createChannelSchema = object({
   name: z
     .string({ required_error: 'Channel name is required' })
-    .min(3, { message: 'Must be at least 3 characters' }),
+    .min(3, { message: 'Must be at least 3 characters' })
+    .trim(),
   description: z.string().optional(),
   isMedia: z.boolean().optional(),
 });
