4e9d5c985a59ef09166729e8240f64dd1652ca64
jjensen-ai <jordon.jensen@live.ca>
2023-03-29 14:04:11 -0400

    branch started

--- components/home/MediaChat.tsx
+++ components/home/MediaChat.tsx
@@ -57,19 +57,19 @@ export default function MediaChat() {
               <ParticipantLoop>
                 <ParticipantTile className='w-12 h-12 flex flex-col justify-center items-center m-4'>
                   {connectionState === ConnectionState.Connected && <ParticipantName className='text-lg font-semibold mt-2'/>}
-                  <VideoTrack source={Track.Source.Camera} className={'rounded-xl mx-2 mb-3'}/>
+                  <VideoTrack source={Track.Source.Camera} className={'rounded-xl mx-2 mb-3'} placeholder={user?.avatar_url!}/>
                   <VideoTrack source={Track.Source.ScreenShare} className={'rounded-xl'}/>
                   <AudioTrack source={Track.Source.Microphone} />
                 </ParticipantTile>  
               </ParticipantLoop>
             </div>
             <div className={`flex flex-row justify-evenly ${mediaControls.mediaControls} mb-5 min-w-0 bg-grey-950 py-3 items-center rounded-xl absolute bottom-1 inset-x-1 mx-auto`}>
-              <TrackToggle showIcon={false} className={'w-7 h-7 bg-grey-900 hover:bg-grey-800 rounded-lg text-lg flex items-center justify-center'} source={Track.Source.Camera}>
+              {connectionState === ConnectionState.Connected && <TrackToggle showIcon={false} className={'w-7 h-7 bg-grey-900 hover:bg-grey-800 rounded-lg text-lg flex items-center justify-center'} source={Track.Source.Camera}>
                 {videoTrack.isCameraEnabled ? (<BsCameraVideo size={22} onClick={() => {setDisplayVideo(true);}}/>) : (<BsCameraVideoOff size={22} onClick={() => {setDisplayVideo(false);}}/>)} 
-              </TrackToggle>
-              <TrackToggle showIcon={false} className={'w-7 h-7 bg-grey-900 hover:bg-grey-800 rounded-lg text-lg flex items-center justify-center'} source={Track.Source.ScreenShare}> 
+              </TrackToggle>}
+              {connectionState === ConnectionState.Connected && <TrackToggle showIcon={false} className={'w-7 h-7 bg-grey-900 hover:bg-grey-800 rounded-lg text-lg flex items-center justify-center'} source={Track.Source.ScreenShare}> 
                 {screenTrack.isScreenShareEnabled ? (<TbScreenShare size={22}/>) : (<TbScreenShareOff size={22}/>) }
-              </TrackToggle>
+              </TrackToggle>}
               {connectionState !== ConnectionState.Connected ? (<button className='w-7 h-7 bg-green-500 hover:bg-green-700 rounded-lg font-bold text-md' onClick={() => {setConnectionState(true), setToken(token);}}> Join </button> ) : (
                 <DisconnectButton className={'w-7 h-7 bg-red-500 hover:bg-red-700 rounded-lg font-bold text-xl'} onClick={() => {setConnectionState(false); }}> End </DisconnectButton>
               )}
