ffc6e0bfdfa9401d0f3157cf9066cdb66b3f53a6
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-03-29 21:00:05 -0400

    remove comment

--- components/home/RenderDesktopView.tsx
+++ components/home/RenderDesktopView.tsx
@@ -170,9 +170,6 @@ export function renderContent(
           ];
         else return [<ServerList key={1} />, <Chat key={2} />];
       return [<ServerList key={1} />, <DefaultTest key={2} />];
-    // case 'messages':
-    //   if (channel) return [<DMessageList key={1} />, <Chat key={2} />];
-    //   return [<DMessageList key={1} />, <DefaultTest key={2} />];
     default:
       return [<FriendsList key={1} />, <DefaultTest key={2} />];
   }
