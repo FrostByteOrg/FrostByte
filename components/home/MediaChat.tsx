8f120b6bf0b6e181295a7621946271fc245f145e
multimokia <multimokia3@gmail.com>
2023-03-29 23:00:15 -0400

    placeholder entrypoint + tagged media

--- components/home/MediaChat.tsx
+++ components/home/MediaChat.tsx
@@ -54,8 +54,9 @@ export default function MediaChat({
   const setRoomName = useSetCurrentRoomName();
   const currentRoom = useCurrentRoomRef();
   const setRoomServerId = useSetCurrentRoomServerId();
-
   const [displayVideo, setDisplayVideo] = useState(false);
+
+  const participants = useParticipants();
 
   const token = useToken(
     process.env.NEXT_PUBLIC_LK_TOKEN_ENDPOINT,
@@ -98,7 +99,7 @@ export default function MediaChat({
                   className={`h-11 w-13 bg-grey-500 rounded-lg ${mediaStyle.mediaError} border-2 opacity-70 flex flex-col items-center justify-center`}
                 >
                   <span className="text-lg font-bold">
-                  You are connected to {currentRoom.name}
+                    You are connected to {currentRoom.name}
                   </span>
                   <span className="text-lg font-bold">Please end your call</span>
                 </div>
@@ -138,36 +139,125 @@ export default function MediaChat({
               </div>
             </div>
             <div className="border-t-2 mx-5 border-grey-700 flex "></div>
-            <div className={'h-full relative'}>
-              <div className={'bg-grey-800 h-full w-full items-center'}>
-                <div className="">
-                  <div className="flex flex-row justify-center flex-wrap p-5">
+            <div className={'h-full'}>
+              <div className={'bg-grey-800 w-full items-center'}>
+                <div className="w-full">
+                  <div className="grid gap-2 grid-cols-2 p-5 space-x-4 overflow-auto">
                     {connectionState === ConnectionState.Connecting ? (
                       <div className="flex flex-row items-center mt-2">
                         <BsGear size={40} className="animate-spin mr-2" />
                         <span>Connecting...</span>
                       </div>
                     ) : (
-                      <ParticipantLoop>
-                        <ParticipantTile className="w-12 h-12 flex flex-col justify-center items-center m-4">
-                          {connectionState === ConnectionState.Connected && (
-                            <ParticipantName className="text-lg font-semibold mt-2" />
-                          )}
-                          <VideoTrack
-                            source={Track.Source.Camera}
-                            className={'rounded-xl mx-2 mb-3'}
-                          />
-                          <VideoTrack
-                            source={Track.Source.ScreenShare}
-                            className={'rounded-xl'}
-                          />
-                          <AudioTrack source={Track.Source.Microphone} />
+                      participants.map((participant) => (
+                        <ParticipantTile key={participant.sid} participant={participant}>
+                          <div>
+                            { (participant.isCameraEnabled || participant.isScreenShareEnabled) ?
+                              (
+                                <div>
+                                  {participant.isCameraEnabled && (
+                                    <div>
+                                      <VideoTrack
+                                        source={Track.Source.Camera}
+                                        participant={participant}
+                                        className={'rounded-lg'}
+                                      />
+                                      <ParticipantName
+                                        participant={participant}
+                                        className="
+                                          text-lg
+                                          font-semibold
+                                          mt-2
+                                          py-1
+                                          px-2
+                                          rounded-md
+                                          bg-slate-900
+                                          text-center
+                                          float-right
+                                          relative
+                                          bottom-7
+                                          right-2
+                                        "
+                                      />
+                                    </div>
+                                  )}
+                                  {participant.isScreenShareEnabled && (
+                                    <div>
+                                      <VideoTrack
+                                        participant={participant}
+                                        source={Track.Source.ScreenShare}
+                                        className={'rounded-lg'}
+                                      />
+                                      <ParticipantName
+                                        participant={participant}
+                                        className="
+                                          text-lg
+                                          font-semibold
+                                          mt-2
+                                          py-1
+                                          px-2
+                                          rounded-md
+                                          bg-slate-900
+                                          text-center
+                                          float-right
+                                          relative
+                                          bottom-7
+                                          right-2
+                                        "
+                                      />
+                                    </div>
+                                  )}
+                                </div>
+                              ) : (
+                                <div className="bg-gray-600 rounded-lg">
+                                  <img
+                                    className='rounded-lg'
+                                    src="https://designshack.net/wp-content/uploads/placehold.jpg"
+                                    alt=""
+                                  />
+                                  <ParticipantName
+                                    participant={participant}
+                                    className="
+                                      text-lg
+                                      font-semibold
+                                      mt-2
+                                      py-1
+                                      px-2
+                                      rounded-md
+                                      bg-slate-900
+                                      text-center
+                                      float-right
+                                      relative
+                                      bottom-7
+                                      right-2
+                                    "
+                                  />
+                                </div>
+                              )
+                            }
+                          </div>
+                          <AudioTrack source={Track.Source.Microphone} participant={participant}/>
                         </ParticipantTile>
-                      </ParticipantLoop>
+                      ))
                     )}
                   </div>
                   <div
-                    className={`flex flex-row justify-evenly ${mediaStyle.mediaControls} mb-5 min-w-0 bg-grey-950 py-3 items-center rounded-xl absolute bottom-1 inset-x-1 mx-auto`}
+                    className={`
+                      flex
+                      flex-row
+                      justify-evenly
+                      ${mediaStyle.mediaControls}
+                      mb-5
+                      min-w-0
+                      bg-grey-950
+                      py-3
+                      items-center
+                      rounded-xl
+                      absolute
+                      bottom-1
+                      inset-x-1
+                      mx-auto
+                    `}
                   >
                     {connectionState === ConnectionState.Connected && (
                       <TrackToggle
