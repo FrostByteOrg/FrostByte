35328433b09636dc8a193a619559838d7994b87b
multimokia <multimokia3@gmail.com>
2023-04-11 16:18:35 -0400

    add splash for when no channel is selected

---
+++ components/home/DefaultSplash.tsx
@@ -0,0 +1,11 @@
+//TODO: REMOVE THIS FILE, JUST FOR TESTING
+
+import { FrostcordSnowflake } from '@/components/icons/FrostcordSnowflake';
+
+export default function DefaultSplash() {
+  return (
+    <div className="w-full h-full flex items-center">
+      <FrostcordSnowflake className='fill-slate-600 w-full h-full' fillColor='#8a94a633'/>
+    </div>
+  );
+}
