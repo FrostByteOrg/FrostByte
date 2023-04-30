fe6501d17afd2a53eafe8e9be975bfc915ce173e
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-04-29 22:20:12 -0400

    test splash

--- components/home/DefaultSplash.tsx
+++ components/home/DefaultSplash.tsx
@@ -5,10 +5,22 @@ export default function DefaultSplash() {
   const servers = useServers();
   return (
     <div className="w-full h-full flex flex-col items-center">
-      <FrostcordSnowflake className='fill-slate-600 w-full h-full' fillColor='#8a94a633'/>
+      <video
+        loop
+        src="./Render_3.mp4"
+        autoPlay={true}
+        muted
+        className="mix-blend-multiply absolute min-w-[1920px] min-h-[1080px] w-full h-full"
+      />
+      <FrostcordSnowflake
+        className="fill-slate-600 w-full h-full"
+        fillColor="#8a94a633"
+      />
       {servers.length === 0 && (
-        <div className='w-full text-center text-gray-400 mb-8 italic animate-pulse'>
-          <h1 className="text-xl">{'Looks like you\'re not a part of any servers...'}</h1>
+        <div className="w-full text-center text-gray-400 mb-8 italic animate-pulse">
+          <h1 className="text-xl">
+            {"Looks like you're not a part of any servers..."}
+          </h1>
           <h2 className="text-xl">{'Why not create or join one?'}</h2>
         </div>
       )}
