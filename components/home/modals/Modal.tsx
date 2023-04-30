a5b52d1a4c5f71abe8a387cab72da923a4ab1031
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-04-30 08:06:42 -0400

    refactor to react portal

--- components/home/modals/Modal.tsx
+++ components/home/modals/Modal.tsx
@@ -1,6 +1,7 @@
-import { useEffect, RefObject } from 'react';
+import { useEffect, RefObject, useRef, useState } from 'react';
 import styles from '@/styles/Livekit.module.css';
 import { KeyboardEventHandler } from 'react';
+import { createPortal } from 'react-dom';
 
 export default function Modal({
   modalRef,
@@ -10,33 +11,42 @@ export default function Modal({
   buttons,
   onKeyDown = undefined,
 }: {
-  modalRef: RefObject<HTMLDialogElement>;
+  modalRef?: RefObject<HTMLDialogElement>;
   showModal: boolean;
   title: string;
   children: JSX.Element;
   buttons: JSX.Element;
-  onKeyDown?: KeyboardEventHandler<HTMLDialogElement> | undefined;
+  onKeyDown?: KeyboardEventHandler<HTMLDivElement> | undefined;
 }) {
-  useEffect(() => {
-    if (showModal && !modalRef.current?.open) {
-      modalRef.current?.showModal();
-    }
-  }, [showModal, modalRef]);
+  const ref = useRef<Element | null>(null);
+  const [mounted, setMounted] = useState(false);
 
-  return (
-    <dialog
-      ref={modalRef}
-      className={`${styles.modal} rounded-lg`}
-      onKeyDown={onKeyDown}
-    >
-      <div className="bg-grey-900 p-4 rounded-lg  z-50 ">
-        <div className="text-2xl font-bold tracking-wider">{title}</div>
-        <div className="px-2 pt-4 pb-4 flex flex-col">{content}</div>
-        <div className=" border-t-2 mx-5 border-grey-700"></div>
-        <div className="flex justify-end space-x-5 items-center mt-4">
-          {buttons}
-        </div>
-      </div>
-    </dialog>
-  );
+  useEffect(() => {
+    ref.current = document.querySelector<HTMLElement>('#modalPortal');
+    setMounted(true);
+  }, []);
+
+  if (!showModal) return null;
+
+  return mounted && ref.current
+    ? createPortal(
+        <>
+          <div className="fixed z-50 top-[0px] left-[0px] right-[0px] bottom-[0px] bg-frost-900 opacity-50"></div>
+          <div
+            className="rounded-lg fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]  p-5 z-50"
+            onKeyDown={onKeyDown}
+          >
+            <div className="bg-grey-900 p-4 rounded-lg  z-50 ">
+              <div className="text-2xl font-bold tracking-wider">{title}</div>
+              <div className="px-2 pt-4 pb-4 flex flex-col">{content}</div>
+              <div className=" border-t-2 mx-5 border-grey-700"></div>
+              <div className="flex justify-end space-x-5 items-center mt-4">
+                {buttons}
+              </div>
+            </div>
+          </div>
+        </>,
+        ref.current
+      )
+    : null;
 }
