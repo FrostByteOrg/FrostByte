c7fe1193482d3e7b5c8bf94ddd28dac6f7884e90
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-05-01 10:15:47 -0400

    scale ink backgrounds window size

--- components/home/modals/Modal.tsx
+++ components/home/modals/Modal.tsx
@@ -1,5 +1,5 @@
 import { useEffect, RefObject, useRef, useState } from 'react';
-import styles from '@/styles/Livekit.module.css';
+import styles from '@/styles/Modal.module.css';
 import { KeyboardEventHandler } from 'react';
 import { createPortal } from 'react-dom';
 import ReactPlayer from 'react-player/file';
@@ -22,27 +22,54 @@ export default function Modal({
   const ref = useRef<Element | null>(null);
   const [mounted, setMounted] = useState(false);
 
+  const ref2 = useRef(null);
+
+  const [videoStatus, setVideoStatus] = useState<
+    'play' | 'pause' | 'playAfter'
+  >('play');
+
   useEffect(() => {
     ref.current = document.querySelector<HTMLElement>('#modalPortal');
     setMounted(true);
   }, []);
 
+  useEffect(() => {
+    if (!showModal) {
+      setVideoStatus('pause');
+    } else {
+      setVideoStatus('play');
+    }
+  }, [showModal]);
+
   if (!showModal) return null;
 
-  //TODO: detect screen size with js and set min-h/w accordingly
+  //TODO: make both close via appropriate buttons, blur background. Start adding button frostpunk styles? Make smaller modal work (add graphic for it)
 
   return mounted && ref.current
     ? createPortal(
         <>
           <ReactPlayer
-            url="./Render_4.mp4"
-            className="mix-blend-multiply fixed z-50 top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] min-w-[90%] min-h-[90%] w-full h-full opacity-95"
-            playing={true}
+            ref={ref2}
+            url="./mainFull.mp4"
+            className={`${styles.bigModal} mix-blend-multiply fixed z-50 top-[46%] left-[54%] translate-y-[-50%] translate-x-[-50%]  w-full h-full opacity-95`}
+            playing={
+              videoStatus == 'play' || videoStatus == 'playAfter' ? true : false
+            }
+            onProgress={({ playedSeconds }) => {
+              if (playedSeconds >= 2.6 && videoStatus !== 'playAfter')
+                setVideoStatus('pause');
+            }}
           />
           <ReactPlayer
-            url="./Render_5.mp4"
-            className="mix-blend-multiply fixed z-50 top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] min-w-[2920px] min-h-[2080px] w-full h-full"
-            playing={true}
+            url="./secondaryFull.mp4"
+            className={`${styles.bigModalSecondary} mix-blend-multiply fixed z-50 top-[46%] left-[54%] translate-y-[-50%] translate-x-[-50%]  w-full h-full opacity-[0.9] `}
+            playing={
+              videoStatus == 'play' || videoStatus == 'playAfter' ? true : false
+            }
+            onProgress={({ playedSeconds }) => {
+              if (playedSeconds >= 2.6 && videoStatus !== 'playAfter')
+                setVideoStatus('pause');
+            }}
           />
 
           <div
@@ -52,6 +79,7 @@ export default function Modal({
             <div className="bg-grey-900 p-4 rounded-lg  z-50 ">
               <div className="text-2xl font-bold tracking-wider">{title}</div>
               <div className="px-2 pt-4 pb-4 flex flex-col">{content}</div>
+              <div onClick={() => setVideoStatus('playAfter')}>play</div>
               <div className=" border-t-2 mx-5 border-grey-700"></div>
               <div className="flex justify-end space-x-5 items-center mt-4">
                 {buttons}
