c8d12526998486423c1c20e161d57a22663e4ad2
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-06-14 18:42:52 -0400

    default splash mobile

--- components/home/mobile/RenderMobileView.tsx
+++ components/home/mobile/RenderMobileView.tsx
@@ -5,18 +5,20 @@ import ServerList from '@/components/home/ServerList';
 import DMessageList from '@/components/home/DMessageList';
 import NavBar from '../NavBar';
 import styles from '@/styles/Home.module.css';
-import { useChannel, useProfile } from '@/lib/store';
+import { useChannel, useProfile, useServers } from '@/lib/store';
 import MediaChat from '../MediaChat';
 import { RoomAudioRenderer } from '@livekit/components-react';
 import GearIcon from '@/components/icons/GearIcon';
 import EditUserModal from '../modals/EditUserModal';
 import { useState } from 'react';
+import DefaultSplash from '../DefaultSplash';
 
 export default function RenderMobileView() {
   const sideBarOption = useSideBarOptionValue();
 
   const channel = useChannel();
   const editUser = useProfile();
+  const servers = useServers();
 
   const [showEditUser, setShowEditUser] = useState(false);
 
@@ -26,33 +28,35 @@ export default function RenderMobileView() {
         <div className=" flex flex-col h-screen">
           <RoomAudioRenderer />
           <div className=" grow overflow-y-clip flex flex-col h-full ">
-            <MediaChat channel={channel}/>
+            <MediaChat channel={channel} />
           </div>
           <div className={`${styles.bottomNav} bg-grey-950 shrink-0`}>
-            <NavBar type='bottom' />
+            <NavBar type="bottom" />
           </div>
         </div>
       );
     }
     return (
-      <div className=' flex flex-col h-screen'>
-        <div className=' grow overflow-y-clip flex flex-col h-full '>
+      <div className=" flex flex-col h-screen">
+        <div className=" grow overflow-y-clip flex flex-col h-full ">
           <Chat />
         </div>
         <div className={`${styles.bottomNav} bg-grey-950 shrink-0`}>
-          <NavBar type='bottom' />
+          <NavBar type="bottom" />
         </div>
       </div>
     );
   }
 
   let mainView = <ServerList />;
+  let mainViewText = '';
   switch (sideBarOption) {
     case 'friends':
       mainView = <FriendsList />;
       break;
     case 'servers':
       mainView = <ServerList />;
+      mainViewText = 'SERVERS';
       break;
     case 'messages':
       mainView = <DMessageList />;
@@ -63,12 +67,17 @@ export default function RenderMobileView() {
   }
 
   return (
-    <div className=' flex flex-col h-screen'>
-      <div className=' grow overflow-y-scroll flex flex-col h-full '>
+    <div className=" flex flex-col h-screen">
+      <div className=" grow overflow-y-scroll flex flex-col h-full ">
         {mainView}
+        {servers.length < 1 && mainViewText == 'SERVERS' ? (
+          <DefaultSplash />
+        ) : (
+          ''
+        )}
       </div>
       <div className={`${styles.bottomNav} bg-grey-950 shrink-0`}>
-        <NavBar type='bottom' />
+        <NavBar type="bottom" />
       </div>
     </div>
   );
